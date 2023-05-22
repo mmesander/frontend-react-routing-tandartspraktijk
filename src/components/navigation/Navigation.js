7a595bc108bde24bcfee5cd09dd1cf51d799bb8b
mmesander <mark_mesander@hotmail.com>
2023-05-23 01:51:05 +0200

    De menu-items in de nav-bar klikbaar gemaakt en onderstreept indien ze actief zijn

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Navigation.css';
+import {NavLink} from "react-router-dom";
 
 function Navigation() {
-  return (
-    <nav>
-      <div className="nav-container">
-        <h4>De Tandenborstel</h4>
+    return (
+        <nav>
+            <div className="nav-container">
+                <h4>De Tandenborstel</h4>
 
-        <ul>
-          <li>Home</li>
-          <li>Gaatjes</li>
-          <li>Afspraak maken</li>
-          <li>Tanden bleken</li>
-        </ul>
-      </div>
-    </nav>
-  );
+                <ul>
+                    <li><NavLink className={({isActive}) => isActive ? 'active-nav-link' : 'default-nav-link'} to="/">Home</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? 'active-nav-link' : 'default-nav-link'} to="/gaatjes">Gaatjes</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? 'active-nav-link' : 'default-nav-link'} to="/afspraken">Afspraak maken</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? 'active-nav-link' : 'default-nav-link'} to="/tanden-bleken">Tanden bleken</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
