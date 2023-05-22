f8e0a407e9c21f6b6a1c42cd39f30cd3de2f3986
mmesander <mark_mesander@hotmail.com>
2023-05-23 01:19:53 +0200

    Routes ingesteld in app.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Home from './pages/home/Home';
 import Appointments from './pages/appointments/Appointments';
 import Cavities from './pages/cavities/Cavities';
 import Navigation from './components/navigation/Navigation';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <Navigation />
-      <Home />
-      <Whitening />
-      <Appointments />
-      <Cavities />
-    </>
-  );
+    return (
+        <>
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<Whitening/>}/>
+                <Route path="/" element={<Appointments/>}/>
+                <Route path="/" element={<Cavities/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
